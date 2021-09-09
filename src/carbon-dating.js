519c03575af15c1372bbc0a72ac427064fade4e9
l1irik-rbk <kir.baranow2010@gmail.com>
2021-09-09 16:19:31 +0300

    feat: add two tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  let n = parseFloat(sampleActivity)
+  if (typeof sampleActivity !== 'string' || isNaN(n) || n <= 0 || n >= 15) return false
+  let k = 0.693 / HALF_LIFE_PERIOD
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / n) / k)
+  return t
   // remove line with error and write your code here
 }
